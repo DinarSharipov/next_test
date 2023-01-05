557e8cc27e39fc58bc32023b27ecaf6e24f5d4fa
Dinar Sharipov <Donotcoverr@mail.ru>
2023-01-05 18:43:53 +0300

    completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import store from '../store/index';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
 
 export default App;
