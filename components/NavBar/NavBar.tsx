7a3cf028665834aca2d49959a7eb23e27196704a
Dinar Sharipov <Donotcoverr@mail.ru>
2023-01-08 21:17:52 +0300

    completed

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../Button';
+
+const NavBar: React.FC = () => (
+  <div className="flex-1 items-center p-2">
+    <div className="flex items-center">
+      <div>Logo</div>
+      <div>
+        <ul className="flex items-center">
+          <Button.Link
+            href="/"
+            title="Ссылка 1"
+          />
+          <Button.Link
+            href="/"
+            title="Ссылка 1"
+          />
+          <Button.Link
+            href="/"
+            title="Ссылка 1"
+          />
+          <Button.Link
+            href="/"
+            title="Ссылка 1"
+          />
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default React.memo(NavBar);
