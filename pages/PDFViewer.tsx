e5253f11f999b4a3979f138722ea91af3a6f9b15
Dinar Sharipov <Donotcoverr@mail.ru>
2023-01-26 00:06:36 +0300

    completed

--- pages/PDFViewer.tsx
+++ pages/PDFViewer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
@@ -7,17 +7,35 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 const PDFViewer: React.FC = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
+  const [fileUrl, setFileUrl] = useState<string>();
+
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.js">
-      <div style={{ height: '750px' }}>
-        <Viewer
-          fileUrl="../../assets/lis.pdf"
-          plugins={[
-            defaultLayoutPluginInstance,
-          ]}
-        />
-      </div>
-    </Worker>
+    <div>
+      <input
+        onChange={(e) => {
+          if (e.target.files) {
+            const url = URL.createObjectURL(e.target.files[0]);
+            console.log(url);
+
+            setFileUrl(url);
+          }
+        }}
+        type="file"
+      />
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.js">
+        <div
+          className="flex"
+          style={{ height: '750px', display: 'flex', backgroundColor: 'red' }}
+        >
+          <Viewer
+            fileUrl={fileUrl || 'sr'}
+            plugins={[
+              defaultLayoutPluginInstance,
+            ]}
+          />
+        </div>
+      </Worker>
+    </div>
   );
 };
 
