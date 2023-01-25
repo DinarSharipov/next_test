9dd4a8ed24b847f14d83dc727047292c60158c56
Dinar Sharipov <Donotcoverr@mail.ru>
2023-01-25 23:55:19 +0300

    в процессе

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../store/index';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Provider store={store}>
