c6cde0561d3689861e85aeb61147d830cfeead56
Dinar Sharipov <Donotcoverr@mail.ru>
2023-01-26 23:21:17 +0300

    correction

--- pages/PDFViewer.tsx
+++ pages/PDFViewer.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { toolbarPlugin, ToolbarSlot } from '@react-pdf-viewer/toolbar';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/toolbar/lib/styles/index.css';
 
 const PDFViewer: React.FC = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   const [fileUrl, setFileUrl] = useState<string>();
+
+  const toolbarPluginInstance = toolbarPlugin();
+  const { Toolbar } = toolbarPluginInstance;
 
   return (
     <div>
@@ -25,8 +30,84 @@ const PDFViewer: React.FC = () => {
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.js">
         <div
           className="flex"
-          style={{ height: '750px', display: 'flex', backgroundColor: 'red' }}
+          style={{ height: '750px' }}
         >
+          <div
+            className="rpv-core__viewer"
+            style={{
+              border: '1px solid rgba(0, 0, 0, 0.3)',
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+            }}
+          >
+            <div
+              style={{
+                alignItems: 'center',
+                backgroundColor: '#eeeeee',
+                borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                display: 'flex',
+                padding: '4px',
+              }}
+            >
+              <Toolbar>
+                {(props: ToolbarSlot) => {
+                  const {
+                    CurrentPageInput,
+                    Download,
+                    EnterFullScreen,
+                    GoToNextPage,
+                    GoToPreviousPage,
+                    NumberOfPages,
+                    Print,
+                    ShowSearchPopover,
+                    Zoom,
+                    ZoomIn,
+                    ZoomOut,
+                  } = props;
+                  return (
+                    <div className="flex">
+                      <div style={{ padding: '0px 2px' }}>
+                        <ShowSearchPopover />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <ZoomOut />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <Zoom />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <ZoomIn />
+                      </div>
+                      <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                        <GoToPreviousPage />
+                      </div>
+                      <div style={{ padding: '0px 2px', width: '4rem' }}>
+                        <CurrentPageInput />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        /
+                        {' '}
+                        <NumberOfPages />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <GoToNextPage />
+                      </div>
+                      <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                        <EnterFullScreen />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <Download />
+                      </div>
+                      <div style={{ padding: '0px 2px' }}>
+                        <Print />
+                      </div>
+                    </div>
+                  );
+                }}
+              </Toolbar>
+            </div>
+          </div>
           <Viewer
             fileUrl={fileUrl || 'sr'}
             plugins={[
@@ -34,6 +115,7 @@ const PDFViewer: React.FC = () => {
             ]}
           />
         </div>
+
       </Worker>
     </div>
   );
